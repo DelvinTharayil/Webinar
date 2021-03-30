15cd42b552c0ce67fab9f00556ff173f3b465f9b
DelvinT <>
2021-03-30 14:07:38 +0530

    committing internal files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <ToolBar/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ToolBar from './components/ToolBar';
+
+export default {
+  name: 'App',
+
+  components: {
+    ToolBar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
